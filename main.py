0b246cde28a03818be6abd982bd8775c305705f3
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2021-01-10 22:41:21 -0600

    mvp working

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
 import os
 import time
+pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Intruders')
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+FONT = pygame.font.SysFont('comicsans', 100)
 
 FPS = 60
 VEL = 5
 BULLET_VEL = 7
 MAX_BULLETS = 4
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 45
-
-RED = (255, 0, 0)
 
 HIT = pygame.USEREVENT + 1
 
@@ -46,13 +49,12 @@ def handle_player_movement(keys_pressed, player):
         player.x += VEL
 
 
-def handle_enemy_movement(enemies, direction):
+def handle_enemy_movement(enemies, direction, player):
     for enemy in enemies:
-        # if direction == 'right' and enemy.x + enemy.width + VEL//3 < WIDTH:
         if direction == 'right':
-            enemy.x += VEL//3
+            enemy.x += VEL/2
         if direction == 'left':
-            enemy.x -= VEL//3
+            enemy.x -= VEL/2
 
 
 def handle_bullets(bullets, enemies):
@@ -82,6 +84,14 @@ def create_enemies(enemies):
                                        SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
 
 
+def draw_conclusion(message, color):
+    text = FONT.render(message, 1, color)
+    WIN.blit(text, (WIDTH/2 - text.get_width() /
+                    2, HEIGHT/2 - text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(5000)
+
+
 def main():
     player = pygame.Rect(WIDTH/2 - SPACESHIP_WIDTH//2, 650,
                          SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -95,6 +105,9 @@ def main():
     run = True
     while run:
         clock.tick(FPS)
+
+        keys_pressed = pygame.key.get_pressed()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -106,20 +119,28 @@ def main():
                         player.x + player.width//2, player.y, 5, 10)
                     bullets.append(bullet)
 
-        keys_pressed = pygame.key.get_pressed()
-        handle_player_movement(keys_pressed, player)
-
         for enemy in enemies:
             if enemy.x + enemy.width + VEL//3 > WIDTH:
                 direction = 'left'
                 for every in enemies:
-                    every.y += every.height//4
+                    every.y += every.height//2
             if enemy.x - VEL//3 < 0:
                 direction = 'right'
                 for every in enemies:
-                    every.y += every.height//4
+                    every.y += every.height//2
+            if enemy.colliderect(player):
+                draw_conclusion('YOU LOST', RED)
+                run = False
+                pygame.quit()
 
-        handle_enemy_movement(enemies, direction)
+        if len(enemies) == 0:
+            draw_conclusion('YOU WON!', GREEN)
+            run = False
+            pygame.quit()
+
+        handle_player_movement(keys_pressed, player)
+
+        handle_enemy_movement(enemies, direction, player)
 
         handle_bullets(bullets, enemies)
 
