85f1b8b5cd1802426a1e44e38ae107f09a8ecd8e
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2021-02-26 08:49:40 -0600

    screenshot

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Space Intruders
 
 A basic clone of the retro arcade game Space Invaders, made using the [pygame](https://www.pygame.org/news) library.
+
+![Screen Shot](./screenshot.png)
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import pygame
 import os
 import time
+
+import pygame
+
 pygame.font.init()
 
+
 WIDTH, HEIGHT = 1000, 700
+
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Space Intruders')
+pygame.display.set_caption("Space Intruders")
+
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-FONT = pygame.font.SysFont('comicsans', 100)
+FONT = pygame.font.SysFont("comicsans", 100)
 
 FPS = 60
 VEL = 5
@@ -19,16 +25,17 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 45
 
 HIT = pygame.USEREVENT + 1
 
-SPACE = pygame.transform.scale(pygame.image.load(
-    os.path.join('assets', 'space.png')), (WIDTH, HEIGHT))
+SPACE = pygame.transform.scale(
+    pygame.image.load(os.path.join("assets", "space.png")), (WIDTH, HEIGHT)
+)
 
-PLAYER_IMAGE = pygame.image.load(os.path.join('assets', 'spaceship_red.png'))
-PLAYER = pygame.transform.rotate(pygame.transform.scale(
-    PLAYER_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 180)
+PLAYER_IMAGE = pygame.image.load(os.path.join("assets", "spaceship_red.png"))
+PLAYER = pygame.transform.rotate(
+    pygame.transform.scale(PLAYER_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 180
+)
 
-ENEMY_IMAGE = pygame.image.load(os.path.join('assets', 'spaceship_yellow.png'))
-ENEMY = pygame.transform.scale(
-    ENEMY_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+ENEMY_IMAGE = pygame.image.load(os.path.join("assets", "spaceship_yellow.png"))
+ENEMY = pygame.transform.scale(ENEMY_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
 
 
 def draw_window(player, enemies, bullets):
@@ -51,10 +58,10 @@ def handle_player_movement(keys_pressed, player):
 
 def handle_enemy_movement(enemies, direction, player):
     for enemy in enemies:
-        if direction == 'right':
-            enemy.x += VEL/2
-        if direction == 'left':
-            enemy.x -= VEL/2
+        if direction == "right":
+            enemy.x += VEL / 2
+        if direction == "left":
+            enemy.x -= VEL / 2
 
 
 def handle_bullets(bullets, enemies):
@@ -71,35 +78,61 @@ def handle_bullets(bullets, enemies):
 def create_enemies(enemies):
     for i in range(40):
         if i < 10:
-            enemies.append(pygame.Rect((i + 1) * (SPACESHIP_WIDTH + 20), 0,
-                                       SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+            enemies.append(
+                pygame.Rect(
+                    (i + 1) * (SPACESHIP_WIDTH + 20),
+                    0,
+                    SPACESHIP_WIDTH,
+                    SPACESHIP_HEIGHT,
+                )
+            )
         if i < 20 and i >= 10:
-            enemies.append(pygame.Rect((i - 9) * (SPACESHIP_WIDTH + 20), SPACESHIP_HEIGHT + 20,
-                                       SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+            enemies.append(
+                pygame.Rect(
+                    (i - 9) * (SPACESHIP_WIDTH + 20),
+                    SPACESHIP_HEIGHT + 20,
+                    SPACESHIP_WIDTH,
+                    SPACESHIP_HEIGHT,
+                )
+            )
         if i < 30 and i >= 20:
-            enemies.append(pygame.Rect((i - 19) * (SPACESHIP_WIDTH + 20), SPACESHIP_HEIGHT * 2 + 40,
-                                       SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+            enemies.append(
+                pygame.Rect(
+                    (i - 19) * (SPACESHIP_WIDTH + 20),
+                    SPACESHIP_HEIGHT * 2 + 40,
+                    SPACESHIP_WIDTH,
+                    SPACESHIP_HEIGHT,
+                )
+            )
         if i < 40 and i >= 30:
-            enemies.append(pygame.Rect((i - 29) * (SPACESHIP_WIDTH + 20), SPACESHIP_HEIGHT * 3 + 60,
-                                       SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+            enemies.append(
+                pygame.Rect(
+                    (i - 29) * (SPACESHIP_WIDTH + 20),
+                    SPACESHIP_HEIGHT * 3 + 60,
+                    SPACESHIP_WIDTH,
+                    SPACESHIP_HEIGHT,
+                )
+            )
 
 
 def draw_conclusion(message, color):
     text = FONT.render(message, 1, color)
-    WIN.blit(text, (WIDTH/2 - text.get_width() /
-                    2, HEIGHT/2 - text.get_height()/2))
+    WIN.blit(
+        text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2)
+    )
     pygame.display.update()
     pygame.time.delay(5000)
 
 
 def main():
-    player = pygame.Rect(WIDTH/2 - SPACESHIP_WIDTH//2, 650,
-                         SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    player = pygame.Rect(
+        WIDTH / 2 - SPACESHIP_WIDTH // 2, 650, SPACESHIP_WIDTH, SPACESHIP_HEIGHT
+    )
     enemies = []
     create_enemies(enemies)
     bullets = []
 
-    direction = 'right'
+    direction = "right"
 
     clock = pygame.time.Clock()
     run = True
@@ -115,26 +148,25 @@ def main():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and len(bullets) < MAX_BULLETS:
-                    bullet = pygame.Rect(
-                        player.x + player.width//2, player.y, 5, 10)
+                    bullet = pygame.Rect(player.x + player.width // 2, player.y, 5, 10)
                     bullets.append(bullet)
 
         for enemy in enemies:
-            if enemy.x + enemy.width + VEL//3 > WIDTH:
-                direction = 'left'
+            if enemy.x + enemy.width + VEL // 3 > WIDTH:
+                direction = "left"
                 for every in enemies:
-                    every.y += every.height//2
-            if enemy.x - VEL//3 < 0:
-                direction = 'right'
+                    every.y += every.height // 2
+            if enemy.x - VEL // 3 < 0:
+                direction = "right"
                 for every in enemies:
-                    every.y += every.height//2
+                    every.y += every.height // 2
             if enemy.colliderect(player):
-                draw_conclusion('YOU LOST', RED)
+                draw_conclusion("YOU LOST", RED)
                 run = False
                 pygame.quit()
 
         if len(enemies) == 0:
-            draw_conclusion('YOU WON!', GREEN)
+            draw_conclusion("YOU WON!", GREEN)
             run = False
             pygame.quit()
 
@@ -147,5 +179,5 @@ def main():
         draw_window(player, enemies, bullets)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
